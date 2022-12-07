0f741718342a45d235bb5bf3a04772c9ccffa6bf
x <exit>
2022-12-07 14:22:22 +0000

    Add foreign key constrait

--- schema.sql
+++ schema.sql
@@ -8,6 +8,30 @@ CREATE TABLE animals (
     escape_attempts integer,
     neutered boolean,
     weight_kg decimal,
-    species varchar,
+    species_id integer,
+    owner_id integer,
+     CONSTRAINT fk_species
+      FOREIGN KEY(species_id) 
+	  REFERENCES species(id)
+	  ON DELETE CASCADE,
+
+      CONSTRAINT fk_owners
+      FOREIGN KEY(owner_id) 
+	  REFERENCES owners(id)
+	  ON DELETE CASCADE,
+
+    primary key(id)
+);
+
+CREATE TABLE owners (
+    id integer GENERATED ALWAYS AS IDENTITY,
+    full_name varchar(100),
+    age integer,
+    primary key(id)
+);
+
+CREATE TABLE species (
+    id integer GENERATED ALWAYS AS IDENTITY,
+    name varchar(100),
     primary key(id)
 );
